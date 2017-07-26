7a801eb8227786a3b69fbb994471df7124709258
Michael Nagels <michaelnagels@hotmail.com>
2017-07-26 11:14:55 +0200

    commit

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -34,22 +34,21 @@ public class InsultGenerator {
                         //res=connection.toString();
                         return res;
                         
-                        //String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
-                        //Statement stmt = connection.createStatement();
-                        //ResultSet rs = stmt.executeQuery(SQL);
-                        //while (rs.next()) {
-                         //   if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
-                          //      article = "a";
-                        //}
-                        //theInsult = String.format("Thou art %s %s %s %s!", article,
-                        //rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+                        String SQL = "select sample as s from audiosamples";
+                        Statement stmt = connection.createStatement();
+                        ResultSet rs = stmt.executeQuery(SQL);
+                        while (rs.next()) {
+                            theInsult += rs.getString("s <br>");
+                        }
+                        theInsult = String.format("Thou art %s %s %s %s!", article,
+                        rs.getString("first"), rs.getString("second"), rs.getString("noun"));
                     }
-                    //rs.close();
+                    rs.close();
                     connection.close();
-                //}
+                }
             } catch (Exception e) {
-                //return "Database connection problem!";
-                return e.toString();
+                return "Database connection problem!";
+                //return e.toString();
               }
             return theInsult;
         }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,9 @@
 <%@page import="org.openshift.InsultGenerator"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -14,5 +17,10 @@
 out.println(new InsultGenerator().generateInsult());
 %>
 </p>
+<form method="post" action="fileupload.jsp" enctype="multipart/form-data">
+Select file to upload:
+<input type="file" name="dataFile" id="fileChooser"/><br/><br/>
+<input type="submit" value="Upload" />
+</form>
 </body>
 </html>
