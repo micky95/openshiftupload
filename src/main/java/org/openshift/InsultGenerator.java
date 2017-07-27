f7a44f42b893576083e7e0f317f2e07f5b4bc08e
Michael Nagels <michaelnagels@hotmail.com>
2017-07-27 16:05:15 +0200

    test

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 
@@ -28,36 +29,32 @@ public class InsultGenerator {
 	public String testConnection() {
 	
                
+                String res="";
                 Connection connection=null;
-                //try{
+                try{
+                    
                     connection = getConnection();
                     if (connection != null) {
 			//return "tlukt!!!";
-                        String res = "tmacheert";
-                        //res=connection.toString();
+                        res += " tmacheert ";
+                        res+=connection.toString();
                         
                         
-                        String SQL = "select sample from audiosamples";
-                        Statement stmt = connection.createStatement();
-    
-                        ResultSet rs = stmt.executeQuery(SQL);
-                        res= rs.toString();
-                        //return res;
-                        while (rs.next()) {
-                            res += "<br>"+rs.getString("sample");
-                        }
+                        String SQL = "insert into audiosamples (sample) values (?)";
+                        PreparedStatement st= connection.prepareStatement(SQL);
+                        st.setInt(1, 1234568);
+                        st.execute();
+                        //Statement stmt = connection.createStatement();
+                        //stmt.executeQuery(SQL);
                         return res;
                         //theInsult = String.format("Thou art %s %s %s %s!", article,
                         //rs.getString("first"), rs.getString("second"), rs.getString("noun"));
                     }
-                    else{
-                    return "database connection error!";
-                    }
                     connection.close();
-                //}
-                //catch(Exception e){
-                  //  return "Database connection error!!";
-                //}
+                }
+                catch(Exception e){
+                    return "Database connection error!!";
+                }
                     //rs.close();
                 return null;
                 
